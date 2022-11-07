99826d514d8b1eee3598a0159e7cc9f47b7b1274
alanz420 <lucasalonso.m94@gmail.com>
2022-11-07 11:53:57 -0300

    json server

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,66 +1,99 @@
-import React from 'react';
-import Main from '../Components/Main/Main';
-import pokemones from '../Api/Data';
-import { useState } from 'react'
-import SearchBar from '../Components/Header/SearchBar/SearchBar';
-import LogoAndButton from '../Components/Header/LogoAndButton/LogoAndButton'
+import React from "react";
+import Main from "../Components/Main/Main";
+// import pokemones from "../Api/Data";
+import { useState, useEffect } from "react";
+import SearchBar from "../Components/Header/SearchBar/SearchBar";
+import LogoAndButton from "../Components/Header/LogoAndButton/LogoAndButton";
 //prueba
 function Home() {
-    const [list, setList] = useState(pokemones);
+  const [list, setList] = useState([]);
+  const [resetear, setResetear] = useState(false);
 
-    const search = (x) => {
-        if (x !== "") {
-            const filtrados = [...list].filter((pokemon) => {
-                if (pokemon.name.toLowerCase().includes(x)) {
-                    return true;
-                } else return false;
-            });
-            setList(filtrados);
-        } else {
-            setList(pokemones);
-        }
-    };
+  useEffect(() => {
+    fetch("http://localhost:3000/pokemones", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setList(data);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }, [resetear]);
 
-    const [active, setActive] = useState(true);
+  /* const traerDatos = () => {
+    fetch("http://localhost:3000/pokemones", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setList(data);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }; */
+  const search = (x) => {
+    if (x !== "") {
+      const filtrados = [...list].filter((pokemon) => {
+        if (pokemon.name.toLowerCase().includes(x)) {
+          return true;
+        } else return false;
+      });
+      setList(filtrados);
+    } else {
+      //Cambio el estado del array de dependecias del useEffect para que se ejecute el fetch nuevamente.
+      setResetear(!resetear);
+    }
+  };
 
-    const sortById = () => {
-        const newId = [...list].sort(function (a, b) {
-            if (a.id > b.id) {
-                return 1;
-            }
-            if (a.id < b.id) {
-                return -1;
-            }
-            return 0;
-        });
-        setList(newId);
-        setActive(true);
-    };
+  const [active, setActive] = useState(true);
 
-    const sortByName = () => {
-        const name = [...list].sort(function (a, b) {
-            if (a.name > b.name) {
-                return 1;
-            }
-            if (a.name < b.name) {
-                return -1;
-            }
-            return 0;
-        });
-        setList(name);
-        setActive(false);
-    };
+  const sortById = () => {
+    const newId = [...list].sort(function (a, b) {
+      if (a.id > b.id) {
+        return 1;
+      }
+      if (a.id < b.id) {
+        return -1;
+      }
+      return 0;
+    });
+    setList(newId);
+    setActive(true);
+  };
 
-    return (
-        <div className='mainAppContainer'>
-            <header>
-                <LogoAndButton sortById={sortById} sortByName={sortByName} active={active} />
-                <SearchBar search={search} />
-            </header>
+  const sortByName = () => {
+    const name = [...list].sort(function (a, b) {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      return 0;
+    });
+    setList(name);
+    setActive(false);
+  };
 
-            <Main list={list} />
-        </div>
-    )
+  return (
+    <div className="mainAppContainer">
+      <header>
+        <LogoAndButton
+          sortById={sortById}
+          sortByName={sortByName}
+          active={active}
+        />
+        <SearchBar search={search} />
+      </header>
+
+      <Main list={list} />
+    </div>
+  );
 }
 
 export default Home;
