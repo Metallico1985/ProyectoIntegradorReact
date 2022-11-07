3bded04a05402c1b7399e43afa434bcbdb7a9dec
Metallico1985 <andrescardonalatorre@gmail.com>
2022-11-07 14:56:55 -0300

    estilo pagina error

--- src/Routes/ErrorComponent.jsx
+++ src/Routes/ErrorComponent.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './Error.css'
+// import image from 'public/Images/pikachuRoto.jpg'
 
 function ErrorComponent() {
     return (
-        <div>Error 404</div>
+        <div className='errorContainer'>
+            <div>
+
+                <p className='error'>Error 404</p>
+                <p>Tecleaste cualquiera papu...</p>
+                <img src="../Images/pikachuRoto.jpg" alt="pikachu todo roto" />
+            </div>
+
+        </div>
     )
 }
 
